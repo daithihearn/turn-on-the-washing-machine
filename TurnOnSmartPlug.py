f8d7439dde5b259d8cc6cb5310740c85099b8f2a
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-26 12:54:31 +0200

    Adding Meross smart plug support

---
+++ TurnOnSmartPlug.py
@@ -0,0 +1,17 @@
+
+import asyncio
+from services.PriceService import get_current_price, get_max_price, get_min_price
+from services.SmartPlugService import turn_on
+
+curr_price = get_current_price()
+min_price = get_min_price()
+max_price = get_max_price()
+
+
+async def main():
+    if curr_price == min_price:
+        await turn_on()
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
---
+++ services/SmartPlugService.py
@@ -0,0 +1,29 @@
+import asyncio
+import os
+from meross_iot.http_api import MerossHttpClient
+from meross_iot.manager import MerossManager
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MEROSS_EMAIL = os.getenv('MEROSS_EMAIL')
+MEROSS_PASSWORD = os.getenv('MEROSS_PASSWORD')
+DEVICE_TO_TURN_ON = os.getenv('DEVICE_TO_TURN_ON')
+
+
+async def turn_on():
+    if (DEVICE_TO_TURN_ON != None):
+        # Setup the HTTP client API from user-password
+        http_api_client = await MerossHttpClient.async_from_user_password(email=MEROSS_EMAIL, password=MEROSS_PASSWORD)
+        # Setup and start the device manager
+        manager = MerossManager(http_client=http_api_client)
+        await manager.async_init()
+
+        # Retrieve all the MSS310 devices that are registered on this account
+        await manager.async_device_discovery()
+        plugs = manager.find_devices(device_type="mss310")
+
+        for plug in plugs:
+            if (plug.name == DEVICE_TO_TURN_ON):
+                print("Turning on " + plug.name)
+                await plug.async_turn_on(channel=0)
