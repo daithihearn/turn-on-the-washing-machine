7eb1133707a552f58acd082030a11ec767108289
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-26 13:05:56 +0200

    Adding logging when we don't turn on the smart plug

--- TurnOnSmartPlug.py
+++ TurnOnSmartPlug.py
@@ -11,6 +11,9 @@ max_price = get_max_price()
 async def main():
     if curr_price == min_price:
         await turn_on()
+    else:
+        print('Not turning the smart plug on. Min price is ' +
+              min_price.value+', current price is '+curr_price.value+'.')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
