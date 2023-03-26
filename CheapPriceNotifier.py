5d4425e243999cc56170dfbf8777df35e3058806
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-26 13:24:45 +0200

    Moving message declaration

--- CheapPriceNotifier.py
+++ CheapPriceNotifier.py
@@ -6,14 +6,10 @@ curr_price = get_current_price()
 min_price = get_min_price()
 max_price = get_max_price()
 
-message = 'Put the washing machine on! Min price: ' + \
-    min_price.value_euro + '@' + min_price.hour + ', Max price: ' + max_price.value_euro + '@' + max_price.hour + \
-    ', Current Price: ' + curr_price.value_euro + '@' + curr_price.hour + '.'
-
-print(message)
-
 if curr_price.value == min_price.value:
-    send_sms(message)
+    send_sms('Put the washing machine on! Min price: ' +
+             min_price.value_euro + '@' + min_price.hour + ', Max price: ' + max_price.value_euro + '@' + max_price.hour +
+             ', Current Price: ' + curr_price.value_euro + '@' + curr_price.hour + '.')
 else:
     print('No need to put the washing machine on. Min price is ' +
           min_price.value_euro+', max price is' + max_price.value_euro+', current price is '+curr_price.value_euro+'.')
--- ExpensivePriceNotifier.py
+++ ExpensivePriceNotifier.py
@@ -6,14 +6,10 @@ curr_price = get_current_price()
 min_price = get_min_price()
 max_price = get_max_price()
 
-message = 'Turn everything off!! Now is the most expensive time. Min price: ' + \
-    min_price.value_euro + '@' + min_price.hour + ', Max price: ' + max_price.value_euro + '@' + max_price.hour + \
-    ', Current Price: ' + curr_price.value_euro + '@' + curr_price.hour + '.'
-
-print(message)
-
 if curr_price.value == max_price.value:
-    send_sms(message)
+    send_sms('Turn everything off!! Now is the most expensive time. Min price: ' +
+             min_price.value_euro + '@' + min_price.hour + ', Max price: ' + max_price.value_euro + '@' + max_price.hour +
+             ', Current Price: ' + curr_price.value_euro + '@' + curr_price.hour + '.')
 else:
     print('No need to warn about the price. Min price is ' +
           min_price.value_euro+', max price is' + max_price.value_euro+', current price is '+curr_price.value_euro+'.')
