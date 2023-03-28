8793ebe42d82ec006045e95fe1f5353afebb8603
Daithi Hearn <daithi.hearn@gmail.com>
2023-03-28 13:32:51 +0200

    Adding cheapest 3 hour period

--- TurnOnSmartPlug.py
+++ TurnOnSmartPlug.py
@@ -1,18 +1,19 @@
 
 import asyncio
-from services.PriceService import get_current_price, get_max_price, get_cheapest_period
+from services.PriceService import get_current_price, get_min_price, get_max_price, get_cheapest_period
 from services.SmartPlugService import turn_on
 
 curr_price = get_current_price()
-min_price = get_cheapest_period(2)
+cheapest_period = get_cheapest_period(3)
+min_price = get_min_price()
 max_price = get_max_price()
 
 
 async def main():
-    if curr_price.value == min_price.value:
+    if curr_price.value == cheapest_period.value:
         await turn_on()
     else:
-        print('Not turning the smart plug on. Min price is ' +
+        print('Not turning the smart plug on. Cheapest 3 hour period starts with '+cheapest_period.value_euro+', min price is ' +
               min_price.value_euro+', max price is'+max_price.value_euro+', current price is '+curr_price.value_euro+'.')
 
 if __name__ == '__main__':
