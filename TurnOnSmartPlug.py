a6f38e98b1d8add246d64496658eee2b899ab1ee
Daithi Hearn <daithi.hearn@gmail.com>
2023-04-27 16:30:21 +0200

    Turn water on every day

--- TurnOnSmartPlug.py
+++ TurnOnSmartPlug.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from services.PriceService import get_current_price, get_min_price, get_max_price, get_cheapest_period, get_today, calculate_average, calculate_day_rating
+from services.PriceService import get_current_price, get_min_price, get_max_price, get_cheapest_period, get_today, calculate_average
 from services.SmartPlugService import turn_on
 from LoggingConfig import configure_logging
 
@@ -18,11 +18,10 @@ min_price = get_min_price(price_data)
 max_price = get_max_price(price_data)
 
 cheapest_period_avg = calculate_average(cheapest_period)
-rating = calculate_day_rating(cheapest_period_avg)
 
 
 async def main():
-    if curr_price.hour == cheapest_period[0].hour and rating != "Malo":
+    if curr_price.hour == cheapest_period[0].hour:
         await turn_on()
     else:
         logging.info(
